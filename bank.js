c423ec258359b588ad7f9a3d952f23d27222563c
Robert Henderson <robert@nterpryz.com>
2018-02-14 15:11:52 +0000

    can find account by name

--- bank.js
+++ bank.js
@@ -6,4 +6,14 @@ Bank.prototype.addAccount = function (account) {
   this.accounts.push(account);
 }
 
+Bank.prototype.accountByName = function(name){
+  let result = null;
+  this.accounts.forEach(function(accountObject){
+    if(accountObject.name === name){
+    result = accountObject;
+  }
+  })
+  return result;
+}
+
 module.exports = Bank;
